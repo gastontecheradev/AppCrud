29b77acdc09a3e29ab47413392d9aa697152ace3
gastontecheradev <acorde.musica10@gmail.com>
2026-05-30 17:18:39 -0300

    Commit

--- AppCrud/Models/Empleado.cs
+++ AppCrud/Models/Empleado.cs
@@ -7,8 +7,8 @@ namespace AppCrud.Models
     {
         [Key]
         public int IdEmpleado { get; set; }
-        public string NombreCompleto { get; set; }
         [MaxLength(50)]
+        public string NombreCompleto { get; set; }
         public string Correo { get; set; }
         public DateOnly FechaContrato { get; set; }
         public bool Activo { get; set; }
